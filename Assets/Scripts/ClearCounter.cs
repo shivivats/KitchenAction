5a09501e712ca02057aef830113957a7d6944aed
Shivi Vats <shivivats7@gmail.com>
2024-10-28 18:46:13 +0100

    let player drop off an object at the clear counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : BaseCounter {
-
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-        
+        if (!HasKitchenObject()) {
+            // there is no kitchen object here
+            if (player.HasKitchenObject())
+                // if the player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+        }
+        else {
+            // there is a kitchen object here
+        }
     }
-
- 
-
-
 }
