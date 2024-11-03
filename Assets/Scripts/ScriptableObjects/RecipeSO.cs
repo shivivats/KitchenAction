f31bd8b506b3c91a3cc75d69f5b2c83a2ece93f4
Shivi Vats <shivivats7@gmail.com>
2024-11-03 09:49:57 +0100

    added files for upcoming scripts because i hate waiting for unity to compile when i add a new script file

---
+++ Assets/Scripts/ScriptableObjects/RecipeSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu()]
+public class RecipeSO : ScriptableObject {
+    public KitchenObjectSO input;
+    public KitchenObjectSO output;
+    public int cuttingAmountMax;
+}
