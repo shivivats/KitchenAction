66c327ed1433f0f049e8c56b773fd642fa12807a
Shivi Vats <shivivats7@gmail.com>
2024-11-03 15:55:10 +0100

    added a recipe list to generate random recipes from for the player to make

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,8 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class DeliveryManager : MonoBehaviour
-{
+public class DeliveryManager : MonoBehaviour {
     
+    // We could have also referenced a list of all the recipes here directly
+    // But this approach has the advantage of if we need another object to refer to all the recipes then we dont need to modify them one by one but rather we can just modify this RecipeListSO
+    [SerializeField] private RecipeListSO recipeListSO;
+    
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake() {
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update() {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer < 0f) {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax) {
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.name);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            }
+        }
+    }
 }
--- Assets/Scripts/ScriptableObjects/RecipeListSO.cs
+++ Assets/Scripts/ScriptableObjects/RecipeListSO.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu()]
+// We commented this out after creating one RecipeListSO so we cannot create another!
+// We only want one of these in our game by design!
+//[CreateAssetMenu()]
 public class RecipeListSO : ScriptableObject {
-    public KitchenObjectSO input;
-    public KitchenObjectSO output;
-    public int cuttingAmountMax;
+    public List<RecipeSO> recipeSOList;
 }
