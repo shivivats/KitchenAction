55ecdf9b484241369f42fb9e3f2ce4d868788a3c
Shivi Vats <shivivats7@gmail.com>
2024-11-03 15:23:13 +0100

    added basic delivery counter

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeliveryCounter : MonoBehaviour
+public class DeliveryCounter : BaseCounter
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                // only accepts plate
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
     }
 }
